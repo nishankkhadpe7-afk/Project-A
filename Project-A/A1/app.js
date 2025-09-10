5365ec091493545d5490386824ed8505ba5458c6
Nishank Khadpe <nishankkhadpe7@gmail.com>
2025-09-10 19:22:53 +0530

    Delete Project-A directory

--- Project-A/A1/app.js
+++
@@ -1,9 +0,0 @@
-function changeText() {
-    document.getElementById("myText").textContent = "My Name is Nishank";
-}
-
-function revertText() {
-    document.getElementById("myText").textContent = "Who are you?";
-}
-
-
